18e126ec36a4d83909f49e1d6b05a07d05566135
ian-IBCIRL <ian@ibcirl.com>
2022-08-25 20:34:05 +0000

    added more scenes, logic and options, including different random starting points on waking up

--- adventure.js
+++ adventure.js
@@ -32,7 +32,7 @@ function startGame() {
     state = {};
 
     // put the first piece of text on the page 
-    showChosenTextItem(1);
+    showChosenTextItem(generateRandom(1,5));
 
     // some fun for the console.
     console.log("Game Starting !!!");
@@ -45,7 +45,15 @@ function startGame() {
 
 //function to select game option
 function selectOption(option) {
+        console.log("Option selected is:", option)
+        // get the next block of story text and options
+        const nextTextChoiceItem = option.nextText
 
+        // update and replace the current state
+        state = Object.assign(state, option.setState)
+
+        // show the next chosen item
+        showChosenTextItem(nextTextChoiceItem)
 }
 
 //function to show particular text item
@@ -53,6 +61,8 @@ function showChosenTextItem(TextItemNumber) {
     // get the desired text for describing the scene
     // array.find(function(currentValue, index, arr),thisValue)
     let textItem = textItems.find(o => o.item === TextItemNumber)
+    // took a while to get this to work - o is a placeholder and => is a function pointer
+    // helpful demo here https://stackoverflow.com/questions/12462318/find-a-value-in-an-array-of-objects-in-javascript
     console.log ("Text Item is: " , textItem)
    
     //log the text for the element describing the scene
@@ -81,9 +91,26 @@ function showChosenTextItem(TextItemNumber) {
 
 function showOption(option) {
     console.log("Showing Option: ", option)
-    return true;
+    // return true if there is no requiredState or the requiredState itself
+    return option.requiredState == null || option.requiredState(state) ;
 }
 
+function generateRandom(min = 0, max = 100) {
+
+    // find diff
+    let difference = max - min;
+
+    // generate random number 
+    let rand = Math.random();
+
+    // multiply with difference 
+    rand = Math.floor( rand * difference);
+
+    // add with min value 
+    rand = rand + min;
+
+    return rand;
+}
 
 // set up the text for the game story line, 
 // describing what the player sees at various stages
@@ -103,7 +130,7 @@ const textItems = [{
             {
                 op: 2,
                 text: "Go back to sleep!",
-                nextText: 7,
+                nextText: generateRandom(1,5),
             },
             {
                 op: 3,
@@ -117,25 +144,131 @@ const textItems = [{
 
             },
         ]
-
     },
     {
         item: 2,
         text: "You wake up on a cold white marble floor, in a small, dimly lit room.",
+        option: [{
+            op: 1,
+            text: "Look around",
+            nextText: 6,
+            setState: {
+                roomlayout: true,
+                hammer: false
+            },
+        },
+        {
+            op: 2,
+            text: "Go back to sleep!",
+            nextText: generateRandom(1,5),
+        },
+        {
+            op: 3,
+            text: "Stand up.",
+            nextText: 8,
+        },
+        {
+            op: 4,
+            text: "Sit up.",
+            nextText: 9,
+
+        },
+    ]
+
     },
     {
         item: 3,
         text: "You wake up on a green marble floor, in a small, dimly lit room.",
+        option: [{
+            op: 1,
+            text: "Look around",
+            nextText: 6,
+            setState: {
+                roomlayout: true,
+                hammer: false
+            },
+        },
+        {
+            op: 2,
+            text: "Go back to sleep!",
+            nextText: generateRandom(1,5),
+        },
+        {
+            op: 3,
+            text: "Stand up.",
+            nextText: 8,
+        },
+        {
+            op: 4,
+            text: "Sit up.",
+            nextText: 9,
+
+        },
+    ]
+
     },
     {
         item: 4,
         text: "You wake up on a rough limestone floor, in a small, dimly lit room.",
+        option: [{
+            op: 1,
+            text: "Look around",
+            nextText: 6,
+            setState: {
+                roomlayout: true,
+                hammer: false
+            },
+        },
+        {
+            op: 2,
+            text: "Go back to sleep!",
+            nextText: generateRandom(1,5),
+        },
+        {
+            op: 3,
+            text: "Stand up.",
+            nextText: 8,
+        },
+        {
+            op: 4,
+            text: "Sit up.",
+            nextText: 9,
+
+        },
+    ]
+
     },
 
     {
         item: 5,
         text: "You wake up on a cold pink granite floor, in a small, dimly lit room.",
-    },
+        option: [{
+            op: 1,
+            text: "Look around",
+            nextText: 6,
+            setState: {
+                roomlayout: true,
+                hammer: false
+            },
+        },
+        {
+            op: 2,
+            text: "Go back to sleep!",
+            nextText: generateRandom(1,5),
+        },
+        {
+            op: 3,
+            text: "Stand up.",
+            nextText: 8,
+        },
+        {
+            op: 4,
+            text: "Sit up.",
+            nextText: 9,
+
+        },
+    ]
+},
     {
         item: 6,
         text: "You see a narrow, barred window, high up on the wall in front of you. You also notice a low hanging light overhead, and a light switch beside a door behind you.",
@@ -172,12 +305,63 @@ const textItems = [{
     },
     {
         item: 7,
-        text: "You wake up on a cold pink granite floor, in a small, dimly lit room.",
-    },
+        text: "You move the flooring and find a hammer.",
+        option: [{
+            op: 1,
+            text: "Pick up the hammer",
+            nextText: 6,
+            setState: {
+                hammer: true
+            },
+        },
+        {
+            op: 2,
+            text: "Ignore the hammer",
+            nextText: 7,
+        },
+        {
+            op: 3,
+            text: "Stand up.",
+            nextText: 8,
+        },
+        {
+            op: 4,
+            text: "Sit up.",
+            nextText: 9,
+
+        },
+    ]
+},
     {
         item: 8,
-        text: "You wake up on a cold pink granite floor, in a small, dimly lit room.",
-    },
+        text: "You stand up, avoiding the low hanging light",
+        option: [{
+            op: 1,
+            text: "Look around",
+            nextText: 6,
+            setState: {
+                roomlayout: true,
+                hammer: false
+            },
+        },
+        {
+            op: 2,
+            text: "Go back to sleep!",
+            nextText: generateRandom(1,5),
+        },
+        {
+            op: 3,
+            text: "Stand up.",
+            nextText: 8,
+        },
+        {
+            op: 4,
+            text: "Sit up.",
+            nextText: 9,
+
+        },
+    ]
+},
     {
         item: 9,
         text: "As you sit up, you notice a loose section of flooring.",
