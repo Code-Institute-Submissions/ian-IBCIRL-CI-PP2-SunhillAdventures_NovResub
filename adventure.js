9440a0a1cbd0d6bf65a52ff0f72be1ab816fb478
ian-IBCIRL <ian@ibcirl.com>
2022-08-26 10:03:16 +0000

    added more WAVE elements

--- adventure.js
+++ adventure.js
@@ -6,8 +6,8 @@ console.log(adventure);
 // This will keep track of our game player and objects etc.
 let state = {};
 
-//getting and setting the initial text - to ensure the app is running
-const textElement = document.getElementById('headertext');
+//getting and setting the initial scene text - to ensure the app is running
+const textElement = document.getElementById('scenetext');
 console.log("text element is:");
 console.log(textElement);
 console.log("text element innertext  is:");
@@ -23,7 +23,7 @@ console.log(optionButtonsElement.innerHTML);
 console.log("Inner Text Length is:", optionButtonsElement.innerText.length);
 
 // react if mouse leaves the div (user may be leaving game)
-function waitDontGo(){
+function waitDontGo() {
     textElement.innerHTML = "Wait !!! Don't GO !"
     console.log("Wait !!! Don't GO !")
 
@@ -39,7 +39,7 @@ function startGame() {
     state = {};
 
     // put the first piece of text on the page 
-    showChosenTextItem(generateRandom(1,5));
+    showChosenTextItem(generateRandom(1, 5));
 
     // some fun for the console.
     console.log("Game Starting !!!");
@@ -52,20 +52,20 @@ function startGame() {
 
 //function to select game option
 function selectOption(option) {
-        console.log("Option selected is:", option)
-        // get the next block of story text and options
-        const nextTextChoiceItem = option.nextText
+    console.log("Option selected is:", option)
+    // get the next block of story text and options
+    const nextTextChoiceItem = option.nextText
 
-        // option to restart game if end not successful
-        if (nextTextChoiceItem <= 0) {
-            return startGame()
-          }
+    // option to restart game if end not successful
+    if (nextTextChoiceItem <= 0) {
+        return startGame()
+    }
 
-        // update and replace the current state
-        state = Object.assign(state, option.setState)
+    // update and replace the current state
+    state = Object.assign(state, option.setState)
 
-        // show the next chosen item
-        showChosenTextItem(nextTextChoiceItem)
+    // show the next chosen item
+    showChosenTextItem(nextTextChoiceItem)
 }
 
 //function to show particular text item
@@ -75,8 +75,8 @@ function showChosenTextItem(TextItemNumber) {
     let textItem = textItems.find(o => o.item === TextItemNumber)
     // took a while to get this to work - o is a placeholder and => is a function pointer
     // helpful demo here https://stackoverflow.com/questions/12462318/find-a-value-in-an-array-of-objects-in-javascript
-    console.log ("Text Item is: " , textItem)
-   
+    console.log("Text Item is: ", textItem)
+
     //log the text for the element describing the scene
     console.log("Text Item is: ", textItem)
     console.log("Text Item Text is: ", textItem.text)
@@ -84,6 +84,9 @@ function showChosenTextItem(TextItemNumber) {
     //set the text for the element describing the scene
     textElement.innerText = textItem.text;
 
+    const legtext = document.getElementById("legendtext")
+    legtext.innerText = `scene #${textItem.item}`
+    
     //clear out the current options, and replace with the options for the current scene
     while (optionButtonsElement.firstChild) {
         optionButtonsElement.removeChild(optionButtonsElement.firstChild);
@@ -104,7 +107,7 @@ function showChosenTextItem(TextItemNumber) {
 function showOption(option) {
     console.log("Showing Option: ", option)
     // return true if there is no requiredState or the requiredState itself
-    return option.requiredState == null || option.requiredState(state) ;
+    return option.requiredState == null || option.requiredState(state);
 }
 
 function generateRandom(min = 0, max = 100) {
@@ -116,7 +119,7 @@ function generateRandom(min = 0, max = 100) {
     let rand = Math.random();
 
     // multiply with difference 
-    rand = Math.floor( rand * difference);
+    rand = Math.floor(rand * difference);
 
     // add with min value 
     rand = rand + min;
@@ -142,7 +145,7 @@ const textItems = [{
             {
                 op: 2,
                 text: "Go back to sleep!",
-                nextText: generateRandom(1,5),
+                nextText: generateRandom(1, 5),
             },
             {
                 op: 3,
@@ -161,93 +164,93 @@ const textItems = [{
         item: 2,
         text: "You wake up on a cold white marble floor, in a small, dimly lit room.",
         option: [{
-            op: 1,
-            text: "Look around",
-            nextText: 6,
-            setState: {
-                roomlayout: true,
-                hammer: false
+                op: 1,
+                text: "Look around",
+                nextText: 6,
+                setState: {
+                    roomlayout: true,
+                    hammer: false
+                },
             },
-        },
-        {
-            op: 2,
-            text: "Go back to sleep!",
-            nextText: generateRandom(1,5),
-        },
-        {
-            op: 3,
-            text: "Stand up.",
-            nextText: 8,
-        },
-        {
-            op: 4,
-            text: "Sit up.",
-            nextText: 9,
+            {
+                op: 2,
+                text: "Go back to sleep!",
+                nextText: generateRandom(1, 5),
+            },
+            {
+                op: 3,
+                text: "Stand up.",
+                nextText: 8,
+            },
+            {
+                op: 4,
+                text: "Sit up.",
+                nextText: 9,
 
-        },
-    ]
+            },
+        ]
 
     },
     {
         item: 3,
         text: "You wake up on a green marble floor, in a small, dimly lit room.",
         option: [{
-            op: 1,
-            text: "Look around",
-            nextText: 6,
-            setState: {
-                roomlayout: true,
-                hammer: false
+                op: 1,
+                text: "Look around",
+                nextText: 6,
+                setState: {
+                    roomlayout: true,
+                    hammer: false
+                },
             },
-        },
-        {
-            op: 2,
-            text: "Go back to sleep!",
-            nextText: generateRandom(1,5),
-        },
-        {
-            op: 3,
-            text: "Stand up.",
-            nextText: 8,
-        },
-        {
-            op: 4,
-            text: "Sit up.",
-            nextText: 9,
+            {
+                op: 2,
+                text: "Go back to sleep!",
+                nextText: generateRandom(1, 5),
+            },
+            {
+                op: 3,
+                text: "Stand up.",
+                nextText: 8,
+            },
+            {
+                op: 4,
+                text: "Sit up.",
+                nextText: 9,
 
-        },
-    ]
+            },
+        ]
 
     },
     {
         item: 4,
         text: "You wake up on a rough limestone floor, in a small, dimly lit room.",
         option: [{
-            op: 1,
-            text: "Look around",
-            nextText: 6,
-            setState: {
-                roomlayout: true,
-                hammer: false
+                op: 1,
+                text: "Look around",
+                nextText: 6,
+                setState: {
+                    roomlayout: true,
+                    hammer: false
+                },
             },
-        },
-        {
-            op: 2,
-            text: "Go back to sleep!",
-            nextText: generateRandom(1,5),
-        },
-        {
-            op: 3,
-            text: "Stand up.",
-            nextText: 8,
-        },
-        {
-            op: 4,
-            text: "Sit up.",
-            nextText: 9,
+            {
+                op: 2,
+                text: "Go back to sleep!",
+                nextText: generateRandom(1, 5),
+            },
+            {
+                op: 3,
+                text: "Stand up.",
+                nextText: 8,
+            },
+            {
+                op: 4,
+                text: "Sit up.",
+                nextText: 9,
 
-        },
-    ]
+            },
+        ]
 
     },
 
@@ -255,32 +258,32 @@ const textItems = [{
         item: 5,
         text: "You wake up on a cold pink granite floor, in a small, dimly lit room.",
         option: [{
-            op: 1,
-            text: "Look around",
-            nextText: 6,
-            setState: {
-                roomlayout: true,
-                hammer: false
+                op: 1,
+                text: "Look around",
+                nextText: 6,
+                setState: {
+                    roomlayout: true,
+                    hammer: false
+                },
             },
-        },
-        {
-            op: 2,
-            text: "Go back to sleep!",
-            nextText: generateRandom(1,5),
-        },
-        {
-            op: 3,
-            text: "Stand up.",
-            nextText: 8,
-        },
-        {
-            op: 4,
-            text: "Sit up.",
-            nextText: 9,
+            {
+                op: 2,
+                text: "Go back to sleep!",
+                nextText: generateRandom(1, 5),
+            },
+            {
+                op: 3,
+                text: "Stand up.",
+                nextText: 8,
+            },
+            {
+                op: 4,
+                text: "Sit up.",
+                nextText: 9,
 
-        },
-    ]
-},
+            },
+        ]
+    },
     {
         item: 6,
         text: "You see a narrow, barred window, high up on the wall in front of you. You also notice a low hanging light overhead, and a light switch beside a door behind you.",
@@ -319,61 +322,61 @@ const textItems = [{
         item: 7,
         text: "You move the flooring and find a hammer.",
         option: [{
-            op: 1,
-            text: "Pick up the hammer",
-            nextText: 6,
-            setState: {
-                hammer: true
+                op: 1,
+                text: "Pick up the hammer",
+                nextText: 6,
+                setState: {
+                    hammer: true
+                },
             },
-        },
-        {
-            op: 2,
-            text: "Ignore the hammer",
-            nextText: 7,
-        },
-        {
-            op: 3,
-            text: "Stand up.",
-            nextText: 8,
-        },
-        {
-            op: 4,
-            text: "Sit up.",
-            nextText: 9,
+            {
+                op: 2,
+                text: "Ignore the hammer",
+                nextText: 7,
+            },
+            {
+                op: 3,
+                text: "Stand up.",
+                nextText: 8,
+            },
+            {
+                op: 4,
+                text: "Sit up.",
+                nextText: 9,
 
-        },
-    ]
-},
+            },
+        ]
+    },
     {
         item: 8,
         text: "You stand up, avoiding the low hanging light",
         option: [{
-            op: 1,
-            text: "Look around",
-            nextText: 6,
-            setState: {
-                roomlayout: true,
-                hammer: false
+                op: 1,
+                text: "Look around",
+                nextText: 6,
+                setState: {
+                    roomlayout: true,
+                    hammer: false
+                },
             },
-        },
-        {
-            op: 2,
-            text: "Go back to sleep!",
-            nextText: generateRandom(1,5),
-        },
-        {
-            op: 3,
-            text: "Stand up.",
-            nextText: 8,
-        },
-        {
-            op: 4,
-            text: "Sit up.",
-            nextText: 9,
+            {
+                op: 2,
+                text: "Go back to sleep!",
+                nextText: generateRandom(1, 5),
+            },
+            {
+                op: 3,
+                text: "Stand up.",
+                nextText: 8,
+            },
+            {
+                op: 4,
+                text: "Sit up.",
+                nextText: 9,
 
-        },
-    ]
-},
+            },
+        ]
+    },
     {
         item: 9,
         text: "As you sit up, you notice a loose section of flooring.",
@@ -403,7 +406,7 @@ const textItems = [{
             {
                 op: 4,
                 text: "Put your head in your hands and try to remember how you got here.",
-                nextText: generateRandom(1,5),
+                nextText: generateRandom(1, 5),
             },
         ]
     },
@@ -411,88 +414,88 @@ const textItems = [{
         item: 10,
         text: "You can just about see out the window and can see the sun rising in the east.",
         option: [{
-            op: 1,
-            text: "Look around",
-            nextText: 6,
-            setState: {
-                timeofday: true
+                op: 1,
+                text: "Look around",
+                nextText: 6,
+                setState: {
+                    timeofday: true
+                },
             },
-        },
-        {
-            op: 2,
-            text: "Do nothing",
-            nextText: 7,
-        },
-        {
-            op: 3,
-            text: "Sit down again",
-            nextText: 9,
-        },
-        {
-            op: 4,
-            text: "Sit up.",
-            nextText: 9,
+            {
+                op: 2,
+                text: "Do nothing",
+                nextText: 7,
+            },
+            {
+                op: 3,
+                text: "Sit down again",
+                nextText: 9,
+            },
+            {
+                op: 4,
+                text: "Sit up.",
+                nextText: 9,
 
-        },
-    ]
-},
-{
-    item: 11,
-    text: "You reach the door and manage to open it gently inwards, although it squeaks loudly. You can hear footsteps coming from the right.",
-    option: [{
-        op: 1,
-        text: "Close the door",
-        nextText: 6,
-        setState: {
-            gaoleralerted: true
-        },
+            },
+        ]
     },
     {
-        op: 2,
-        text: "Do nothing",
-        nextText: 7,
-    },
-    {
-        op: 3,
-        text: "Sit down again",
-        nextText: 9,
-    },
-    {
-        op: 4,
-        text: "Sit up.",
-        nextText: 9,
+        item: 11,
+        text: "You reach the door and manage to open it gently inwards, although it squeaks loudly. You can hear footsteps coming from the right.",
+        option: [{
+                op: 1,
+                text: "Close the door",
+                nextText: 6,
+                setState: {
+                    gaoleralerted: true
+                },
+            },
+            {
+                op: 2,
+                text: "Do nothing",
+                nextText: 7,
+            },
+            {
+                op: 3,
+                text: "Sit down again",
+                nextText: 9,
+            },
+            {
+                op: 4,
+                text: "Sit up.",
+                nextText: 9,
 
-    },
-]
-},    {
-    item: 12,
-    text: "You can just about see out the window and can see the sun rising in the east.",
-    option: [{
-        op: 1,
-        text: "Look around",
-        nextText: 6,
-        setState: {
-            timeofday: true
-        },
-    },
-    {
-        op: 2,
-        text: "Do nothing",
-        nextText: 7,
-    },
-    {
-        op: 3,
-        text: "Sit down again",
-        nextText: 9,
-    },
-    {
-        op: 4,
-        text: "Sit up.",
-        nextText: 9,
+            },
+        ]
+    }, {
+        item: 12,
+        text: "You can just about see out the window and can see the sun rising in the east.",
+        option: [{
+                op: 1,
+                text: "Look around",
+                nextText: 6,
+                setState: {
+                    timeofday: true
+                },
+            },
+            {
+                op: 2,
+                text: "Do nothing",
+                nextText: 7,
+            },
+            {
+                op: 3,
+                text: "Sit down again",
+                nextText: 9,
+            },
+            {
+                op: 4,
+                text: "Sit up.",
+                nextText: 9,
 
+            },
+        ]
     },
-]
-},
 ]
 
 
